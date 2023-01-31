e6cadb81306ea16150e8ce48184a46c405167d90
Márty <986532741@seznam.cz>
2023-01-31 20:42:16 +0100

    render

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const PORT = 5001;
 const io = require('socket.io')(server, {
     cors: {
         //http://localhost:3000
-        origin: "https://pwachat.onrender.com",
+        origin: "https://pwachat-api.onrender.com",
         methods: ['GET', 'POST']
     }
 })
